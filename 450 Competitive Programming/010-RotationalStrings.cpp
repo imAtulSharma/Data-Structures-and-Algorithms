869a4302400125a81f2ee3d2bfad60ad16fcff16
Atul Sharma <atulsharma20may@gmail.com>
2022-01-08 16:23:17 +0530

    Add: New problem

---
+++ 450 Competitive Programming/010-RotationalStrings.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    cout << "Enter two strings less than 30 letters: " << endl;
+    char str1[30], str2[30];
+    cin >> str1 >> str2;
+
+    int start, i = 0;
+    for (; str2[i] != '\0'; i++)
+    {
+        if
+    }
+    if (str2[i] == '\0')
+        return 0;
+    
+
+    return 0;
+}
