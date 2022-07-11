500ecd95e6fe9a055f72e9dfee4c0fb88edc6f56
Atul Sharma <atulsharma20may@gmail.com>
2022-07-11 10:39:32 +0530

    Min Cost Climbing Stairs

--- LeetCode/MinCostClimbingStairs.java
+++ LeetCode/MinCostClimbingStairs.java
@@ -9,13 +9,16 @@ Return the minimum cost to reach the top of the floor.
 class Solution {
     int[] cost;
     Map<Integer, Integer> map = new HashMap<>();
+
     public int minCostClimbingStairs(int[] cost) {
         this.cost = cost;
+
         for(int i = cost.length-1; i > -1; i--) {
             if(i==cost.length-1) map.put(i, cost[i]);
             else if (i == cost.length-2) map.put(i, cost[i]);
             else map.put(i, cost[i] + Math.min(map.get(i+1), map.get(i+2)));
         }
+
         return Math.min(map.get(0), map.get(1));
     }
 }
